d03a74b16c7e869dfbd312259f7f27e37dad7ea1
Jason Nutt <jnutt367@gmail.com>
2021-04-05 16:02:41 -0400

    new shark behavior

--- gsap.js
+++ gsap.js
@@ -4,8 +4,8 @@ gsap.to(".shark", { x: -100, y: 400 });
 
 gsap.to(".text", { duration: 4, y: -200 });
 
-gsap.to(".distance-shark", { duration: 1, x: 100, y: 550 });
+gsap.to(".distance-shark", { duration: 1, x: 100, y: 600 });
 
 gsap.to(".bird", { duration: 6, x: -100, y: 300 });
 
-gsap.to(".birds", { duration: 6, x: 70, y: 400 });
+gsap.to(".birds", { duration: 6, x: 100, y: 400 });
