af5a58ba7ed0f49bef3fe2d95799e868bcad257e
Jason Nutt <jnutt367@gmail.com>
2021-04-05 11:26:47 -0400

    commit for first draft of Ocean discovery

--- gsap.js
+++ gsap.js
@@ -1,2 +1,7 @@
-gsap.to(".fish", { duration: 2, x: 300 });
-console.log("gsap");
+gsap.to(".fish", { duration: 2, x: 200 });
+
+gsap.to(".shark", { x: -200 });
+
+gsap.to(".text", { duration: 3, y: -200 });
+
+gsap.to(".distance-shark", { y: 450 })
