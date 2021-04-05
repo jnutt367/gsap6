f6e24cd25775766d8e92d13eb4b09adb5033c0e7
Jason Nutt <jnutt367@gmail.com>
2021-04-05 14:19:33 -0400

    commit with new ocean graphic placeholder

--- gsap.js
+++ gsap.js
@@ -1,11 +1,11 @@
-gsap.to(".fish", { duration: 2, x: 100, y: 530 });
+gsap.to(".fish", { duration: 2, x: 100, y: 570 });
 
-gsap.to(".shark", { x: -100, y: 430 });
+gsap.to(".shark", { x: -100, y: 530 });
 
 gsap.to(".text", { duration: 4, y: -200 });
 
-gsap.to(".distance-shark", { duration: 5, y: 630 });
+gsap.to(".distance-shark", { duration: 5, x: -130, y: 600 });
 
-gsap.to(".bird", { duration: 6, x: -100, y: 200 });
+gsap.to(".bird", { duration: 6, x: -100, y: 300 });
 
-gsap.to(".birds", { duration: 6, x: 70, y: 100 });
+gsap.to(".birds", { duration: 6, x: 70, y: 400 });
