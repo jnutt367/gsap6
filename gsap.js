c2e6f62f1385c4358b787d3e128a272127af015d
Jason Nutt <jnutt367@gmail.com>
2021-04-05 14:43:46 -0400

    commit for mobile layout animations

--- gsap.js
+++ gsap.js
@@ -1,10 +1,10 @@
-gsap.to(".fish", { duration: 2, x: 100, y: 570 });
+gsap.to(".fish", { duration: 2, x: 100, y: 540 });
 
-gsap.to(".shark", { x: -100, y: 530 });
+gsap.to(".shark", { x: -100, y: 400 });
 
 gsap.to(".text", { duration: 4, y: -200 });
 
-gsap.to(".distance-shark", { duration: 5, x: -130, y: 600 });
+gsap.to(".distance-shark", { duration: 1, x: 100, y: 550 });
 
 gsap.to(".bird", { duration: 6, x: -100, y: 300 });
 
