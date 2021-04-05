97147918219214bd3507f91cab4ea1560450ca8e
Jason Nutt <jnutt367@gmail.com>
2021-04-05 12:12:17 -0400

    commit new shark motion

--- gsap.js
+++ gsap.js
@@ -1,7 +1,7 @@
 gsap.to(".fish", { duration: 2, x: 100 });
 
-gsap.to(".shark", { x: -200 });
+gsap.to(".shark", { x: -100 });
 
 gsap.to(".text", { duration: 3, y: -200 });
 
-gsap.to(".distance-shark", { duration: 3, y: 450 });
+gsap.to(".distance-shark", { duration: 5, y: 450 });
