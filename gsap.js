693f7074d79ba2cb53a114cc76e6184f16d16143
Jason Nutt <jnutt367@gmail.com>
2021-04-05 13:44:49 -0400

    commit for newest version with birds

--- gsap.js
+++ gsap.js
@@ -1,7 +1,11 @@
-gsap.to(".fish", { duration: 2, x: 100 });
+gsap.to(".fish", { duration: 2, x: 100, y: 530 });
 
-gsap.to(".shark", { x: -100 });
+gsap.to(".shark", { x: -100, y: 430 });
 
-gsap.to(".text", { duration: 3, y: -200 });
+gsap.to(".text", { duration: 4, y: -200 });
 
-gsap.to(".distance-shark", { duration: 5, y: 450 });
+gsap.to(".distance-shark", { duration: 5, y: 630 });
+
+gsap.to(".bird", { duration: 6, x: -100, y: 200 });
+
+gsap.to(".birds", { duration: 6, x: 70, y: 100 });
