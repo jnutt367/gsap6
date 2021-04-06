8471a550e3e95116e7c27a85bd3b3bf9dfc5b6fa
Jason Nutt <jnutt367@gmail.com>
2021-04-06 16:04:37 -0400

    commit with new fish instead of shark

--- gsap.js
+++ gsap.js
@@ -1,10 +1,10 @@
 gsap.to(".fish", { duration: 2, x: 100, y: 540 });
 
-gsap.to(".shark", { x: -100, y: 400 });
+gsap.to(".shark", { x: -100, y: 500 });
 
 gsap.to(".text", { duration: 4, y: -200 });
 
-gsap.to(".distance-shark", { duration: 1, x: 100, y: 600 });
+gsap.to(".distance-shark", { duration: 1, x: -10, y: 800 });
 
 gsap.to(".bird", { duration: 6, x: -100, y: 300 });
 
