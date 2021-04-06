e026d0970f4a25ca13a103d4ecd38a701404540c
Jason Nutt <jnutt367@gmail.com>
2021-04-06 18:03:19 -0400

    commit for custom ease

--- gsap.js
+++ gsap.js
@@ -2,10 +2,10 @@ gsap.to(".fish", { duration: 2, x: 100, y: 540 });
 
 gsap.to(".shark", { x: -100, y: 500 });
 
-gsap.to(".text", { duration: 4, y: -200 });
+gsap.to(".text", { duration: 4, y: -200, scale: 1.2, ease: "back" });
 
 gsap.to(".distance-shark", { duration: 1, x: -10, y: 800 });
 
-gsap.to(".bird", { duration: 6, x: -100, y: 300 });
+gsap.to(".bird", { duration: 6, x: -100, y: 300, scale: 6, ease: "back" });
 
 gsap.to(".birds", { duration: 6, x: 100, y: 400 });
