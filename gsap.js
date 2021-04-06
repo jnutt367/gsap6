31f4ce2e41836002397becfc53026e4905018885
Jason Nutt <jnutt367@gmail.com>
2021-04-06 18:07:41 -0400

    commit new bird position

--- gsap.js
+++ gsap.js
@@ -6,6 +6,6 @@ gsap.to(".text", { duration: 4, y: -200, scale: 1.2, ease: "back" });
 
 gsap.to(".distance-shark", { duration: 1, x: -10, y: 800 });
 
-gsap.to(".bird", { duration: 6, x: -100, y: 300, scale: 6, ease: "back" });
+gsap.to(".bird", { duration: 6, x: -100, y: 500, scale: 6, ease: "back" });
 
 gsap.to(".birds", { duration: 6, x: 100, y: 400 });
